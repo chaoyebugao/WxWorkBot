90921be1e36e0345d5a382c191ac6e4f28bce37f
黄盛 <huangsheng@glbdata.com>
2023-02-02 10:02:35 +0800

    修正Key说明

--- Tests/WxWorkRobot.MsDependency.Tests/Startup.cs
+++ Tests/WxWorkRobot.MsDependency.Tests/Startup.cs
@@ -6,7 +6,8 @@ namespace WxWorkRobot.MsDependency.Tests
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddWxWorkBotService("you-webhook-key-here");
+            services.AddWxWorkBotService("you-webhook-key-here");
+            //services.AddWxWorkBotService("a693c72d-8c65-4462-81b0-20220970a3b6");
         }
     }
 }
